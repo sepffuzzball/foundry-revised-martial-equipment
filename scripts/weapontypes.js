1ea2c173e73e81f6932837584d9f50b5da8f9b6b
Sepfy <sepffuzzball@gmail.com>
2024-03-31 12:30:36 -0400

    Added weapon types and properties

---
+++ scripts/weapontypes.js
@@ -0,0 +1,18 @@
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponTypes['ambush'] = 'Ambush';
+    CONFIG.DND5E.weaponTypes['axes'] = 'Axes';
+    CONFIG.DND5E.weaponTypes['bludgeons'] = 'Bludgeons';
+    CONFIG.DND5E.weaponTypes['combatblades'] = 'Combat Blades';
+    CONFIG.DND5E.weaponTypes['duelingblades'] = 'Dueling Blades';
+    CONFIG.DND5E.weaponTypes['flails'] = 'Flails';
+    CONFIG.DND5E.weaponTypes['hammersandpicks'] = 'Hammers and Picks';
+    CONFIG.DND5E.weaponTypes['naturalweapons'] = 'Natural Weapons';
+    CONFIG.DND5E.weaponTypes['polearms'] = 'Polearms';
+    CONFIG.DND5E.weaponTypes['spears'] = 'Spears';
+    CONFIG.DND5E.weaponTypes['whips'] = 'Whips';
+
+    CONFIG.DND5E.weaponTypes['bows'] = 'Bows';
+    CONFIG.DND5E.weaponTypes['crossbows'] = 'Crossbows';
+    CONFIG.DND5E.weaponTypes['firearms'] = 'Firearms';
+    CONFIG.DND5E.weaponTypes['launchweapons'] = 'Launch Weapons';
+});
